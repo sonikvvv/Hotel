9f8731db60faa09e420c25b3b9c6774a8538cf76
Veselin Nikolov <veselinnikolov11@gmail.com>
2020-11-01 19:30:30 +0200

    Additional changes.

--- hotel-project/src/main/java/module-info.java
+++ hotel-project/src/main/java/module-info.java
@@ -2,7 +2,11 @@ module hotel {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires com.jfoenix;
 
     opens hotel to javafx.fxml;
     exports hotel;
+
+    opens gui to javafx.fxml;
+    exports gui;
 }
