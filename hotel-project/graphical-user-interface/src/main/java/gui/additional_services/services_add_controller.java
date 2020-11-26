1ab57d49430faf5e3e620c055c771578f6839815
KaloyanKyuchukov <kyuchukov.kaloyan@gmail.com>
2020-11-26 20:01:25 +0200

    FXML reservation - adding and choosing categories

---
+++ hotel-project/graphical-user-interface/src/main/java/gui/additional_services/services_add_controller.java
@@ -0,0 +1,17 @@
+package gui.additional_services;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class services_add_controller {
+    
+
+    @FXML
+    private ComboBox Service;
+    private ComboBox Category;
+    private TextField Quantity;
+    private TextField Price;
+    private TextArea Note;
+}
---
+++ hotel-project/graphical-user-interface/src/main/java/gui/additional_services/services_category_add_controller.java
@@ -0,0 +1,75 @@
+package gui.additional_services;
+
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class services_category_add_controller {
+
+    @FXML
+    private AnchorPane mainPanel;
+
+    private AnchorPane otherpane;
+
+    @FXML
+    private TextField services_category_add_name;
+
+    @FXML
+    private Button services_category_add_savebutton;
+
+    @FXML
+    private ComboBox<String> services_category_add_negorpos;
+    private ObservableList<String> choose = FXCollections.observableArrayList("Negative", "Positive");
+
+    @FXML
+    void textBoxKeyPressed(KeyEvent event) {
+        String check = services_category_add_name.getText();
+        if (check.length() > 0) {
+            services_category_add_savebutton.setText("Save");
+        } else {
+            services_category_add_savebutton.setText("Skip");
+        }
+
+    }
+
+    @FXML
+    public void initialize() {
+        services_category_add_negorpos.setItems(choose);
+    }
+
+    @FXML
+    void saveOrSkip(ActionEvent event) throws IOException {
+
+        if (services_category_add_savebutton.getText().equals("Save")) {
+            String text = services_category_add_name.getText();
+            String box = services_category_add_negorpos.getValue();
+            if (box==null || box.length() == 0) {
+                Alert al = new Alert(AlertType.WARNING, "Don't leave empty fields!");
+                al.showAndWait();
+            }
+
+        }
+        else
+        {
+            otherpane = FXMLLoader.load(getClass().getResource("services_add.FXML"));
+            mainPanel.getChildren().setAll(otherpane);
+        }
+    }
+
+    
+    
+
+
+
+}
