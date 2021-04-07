c7be2e8307268eaea2ab4d2a558a6bbc1a6a12f4
sonikvvv <51079681+sonikvvv@users.noreply.github.com>
2021-04-07 20:12:05 +0300

    Delete GUI/Hotel Aplication directory

--- GUI/Hotel Aplication/src/Hotel/ReservationsController.java
+++
@@ -1,4 +0,0 @@
-package Hotel;
-
-public class ReservationsController {
-}
--- GUI/Hotel Aplication/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module Hotel.Aplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens Hotel;
-}
